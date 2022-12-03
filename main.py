3b65781f33fc6c9c52f47a3c6c8a52797cba835e
DESKTOP-16C8CU5\bjoer <langb4@bfh.ch>
2022-12-03 23:07:57 +0100

    delete added, non functional

--- main.py
+++ main.py
@@ -1,26 +1,44 @@
-from flask import Flask, request
+from flask import Flask, request, abort
 from flask_restful import Api, Resource, reqparse
 
 app = Flask(__name__)
 api = Api(app)
 
-parser = reqparse.RequestParser()
-parser.add_argument('name', type=str, help='name required..', location='form')
-parser.add_argument('text', type=str, help='text required..', location='form')
-parser.add_argument('zip', type=int, help='zip code required..', location='form')
+sight_parser = reqparse.RequestParser()
+sight_parser.add_argument('name', type=str, help='name required..', location='form', required=True)
+sight_parser.add_argument('text', type=str, help='text required..', location='form', required=True)
+sight_parser.add_argument('zip', type=int, help='zip code required..', location='form', required=True)
 
 sights = {}
+
+def abort_if_not_exists(sight_id):
+    if sight_id not in sights:
+        abort(404, 'sight_id not valid...')
+
+def abort_if_exists(sight_id):
+    if sight_id in sights:
+        abort(409, 'sight_id already exists...')
 
 class Sights(Resource):
 
     def get(self, sight_id):
+        abort_if_not_exists(sight_id)
         return sights[sight_id]
+
     
     def put(self, sight_id):
+        abort_if_exists(sight_id)
         print('FORM---->',request.form)
-        args = parser.parse_args()
+        args = sight_parser.parse_args()
         print('ARGS---->', args)
-        return {sight_id: args}
+        sights[sight_id] = args
+        return sights[sight_id], 201
+
+    def delete(self, sight_id):
+        abort_if_not_exists(sight_id)
+        del sights[sight_id]
+        return sight_id + 'deleted', 204
+
 
 
 api.add_resource(Sights, '/sight/<sight_id>')
--- test.py
+++ test.py
@@ -3,9 +3,19 @@ import requests
 
 BASE = 'http://127.0.0.1:5000/'
 
-response = requests.put(BASE + 'sight/1', {'name': 'Bundeshaus'})#, 'text': 'sch schono schön so', 'zip': 3012})
 
+data = [{'name': 'Bundeshaus', 'text': 'sch schono schoen so', 'zip': 3012},
+        {'name': 'KIndlifresserbrunnen', 'text': 'Hmmm.. chindli', 'zip': 3001},
+        {'name': 'Rosengarten', 'text': 'Guns n rosengardens', 'zip': 3082},]
 
+for i in range(len(data)):
+    response = requests.put(BASE + 'sight/'+str(i), data[i])
+    print('RESPONSE, PUT---->', response.json())
 
-print('RESPONSE---->', response.json())
+input('press enter to delete')
+response = requests.delete(BASE + 'video/1')
+print('RESPONSE, DELETE---->', response)
 
+input('press enter to get')
+response = requests.get(BASE + 'sight/1')
+print('RESPONSE, GET---->', response.json())
