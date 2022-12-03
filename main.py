c562c6d32ea993f06ea513909b23f5957346f220
DESKTOP-16C8CU5\bjoer <langb4@bfh.ch>
2022-12-03 22:20:10 +0100

    added parser for put

--- main.py
+++ main.py
@@ -4,25 +4,26 @@ from flask_restful import Api, Resource, reqparse
 app = Flask(__name__)
 api = Api(app)
 
-sight_put_args = reqparse.RequestParser()
-sight_put_args.add_argument('name', type=str, help='name required..', location='args')
-sight_put_args.add_argument('text', type=str, help='text required..', location='args')
-sight_put_args.add_argument('zip', type=int, help='zip code required..', location='args')
+parser = reqparse.RequestParser()
+parser.add_argument('name', type=str, help='name required..', location='form')
+parser.add_argument('text', type=str, help='text required..', location='form')
+parser.add_argument('zip', type=int, help='zip code required..', location='form')
 
 sights = {}
 
 class Sights(Resource):
+
     def get(self, sight_id):
         return sights[sight_id]
     
     def put(self, sight_id):
         print('FORM---->',request.form)
-        args = sight_put_args.parse_args()
+        args = parser.parse_args()
         print('ARGS---->', args)
         return {sight_id: args}
 
 
-api.add_resource(Sights, '/sight/<int:sight_id>')
+api.add_resource(Sights, '/sight/<sight_id>')
 
 if __name__ == '__main__':
     app.run(debug= True)
--- test.py
+++ test.py
@@ -5,5 +5,7 @@ BASE = 'http://127.0.0.1:5000/'
 
 response = requests.put(BASE + 'sight/1', {'name': 'Bundeshaus'})#, 'text': 'sch schono schön so', 'zip': 3012})
 
+
+
 print('RESPONSE---->', response.json())
 
